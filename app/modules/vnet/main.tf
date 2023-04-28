668c64941a30d3b335b82634e62f1aabec5b992b
ccseyhan <ccseyhan@gmail.com>
2023-04-28 16:31:23 -0300

    Delete app directory

--- app/modules/vnet/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.name
-  address_space       = var.address_space
-  location            = var.location
-  resource_group_name = var.rg_name
-}
